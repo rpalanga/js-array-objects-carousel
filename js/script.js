c25db04d343f486e48e4a978af34a359073339f7
roberto-palanga <r.palanga94@gmail.com>
2024-02-14 19:32:32 +0100

    inserimento delle immagini in java completato

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 Milestone 0:
 Come nel primo carosello realizzato,
  focalizziamoci prima sulla creazione del markup statico: 
- costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+ costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider. 
 
 Milestone 1:
 Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti
@@ -29,3 +29,66 @@ Aggiungere bottoni di start / stop e di inversione del meccanismo di autoplay.
 
 
 */
+
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    },
+    {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    },
+    {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    },
+    {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    },
+    {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+
+const contImage = document.querySelector("#mainImage");
+const slidercontainerImage = document.querySelector("#Slider-image");
+
+
+for (let i = 0; i < images.length; i++) {
+
+    const img = images[i].image;
+    const titolo = images[i].title;
+    const testo = images[i].text;
+
+    contImage.innerHTML += `
+    <div class="Contain-Element">
+        <img src="./${img}" class="w-100 h-100 object-fit-cover">
+        <h2>${titolo}</h2>
+        <p>${testo}</p>
+
+    </div>
+     
+    `
+    slidercontainerImage.innerHTML +=`
+    <div class="Contain-slider my-h" >
+        <img src="./${img}" class="img-fluid w-100 h-100 object-fit-cover">
+        
+    </div>
+    `
+
+}
+
+
+
+
+
+
